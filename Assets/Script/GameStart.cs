b65bc4916b88fd8cb238fe079b4f97152500cfc0
billwillman <billwillman@qq.com>
2024-12-31 02:01:19 +0800

    2222

---
+++ Assets/Script/GameStart.cs
@@ -0,0 +1,239 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using XLua;
+
+namespace SOC.GamePlay
+{
+
+    [LuaCallCSharp]
+    public class GameStart : MonoBehaviour
+    {
+
+        public static GameStart Instance = null;
+        private LuaFunction m_LuaUpdateFunc = null;
+
+        [DoNotGen]
+        public bool m_DS_OutputOldLogHandle = true;
+
+        [DoNotGen]
+        public bool m_ClientOutputOldLogHandle = true;
+
+        [DoNotGen]
+        public bool m_LogWriteAsync = true;
+
+        // Start is called before the first frame update
+        void Awake() {
+            DontDestroyOnLoad(this.gameObject);
+            m_LuaLoaderCallBack = new LuaEnv.CustomLoader(OnLuaFileLoad);
+            Instance = this;
+        }
+
+        private void Start() {
+            ServerAttachLogFile();
+            InitLuaSearchFormatPath();
+            ResourceMgr.Instance.LoadConfigs(OnResConfigResult, this,
+#if UNITY_WEIXINMINIGAME
+                false
+                #else
+                true
+#endif
+                );
+        }
+
+        void InitLuaSearchFormatPath() {
+            string luaSearchPath;
+            if (IsDS) {
+                luaSearchPath = Application.dataPath + "/Lua/{0}.lua.bytes";
+            } else {
+                if (Application.platform == RuntimePlatform.WindowsPlayer || Application.platform == RuntimePlatform.LinuxPlayer)
+                    luaSearchPath = Application.dataPath + "/Lua/{0}.lua.bytes";
+                else
+                    luaSearchPath = Application.persistentDataPath + "/Lua/{0}.lua.bytes";
+            }
+#if !UNITY_EDITOR
+            if (!string.IsNullOrEmpty(luaSearchPath)) {
+                Debug.LogFormat("AddDynicLuaSearchPath: {0}", luaSearchPath);
+                _cLuaRootPathFormats.Add(new LuaPathFormatData(luaSearchPath, true));
+            }
+#endif
+            _cLuaRootPathFormats.Add(new LuaPathFormatData("Resources/@Lua/{0}.lua.bytes", false));
+            _cLuaRootPathFormats.Add(new LuaPathFormatData("Resources/@Lua/_BehaviourTree/{0}.lua.bytes", false));
+        }
+
+        void OnResConfigResult(bool isOk) {
+            if (isOk) {
+                OnInit();
+            } else {
+#if UNITY_EDITOR
+                // 编辑器模式一定会成功
+                OnInit();
+#endif
+            }
+        }
+
+        void OnInit() {
+            InitLuaEnv();
+        }
+
+        void ServerAttachLogFile() {
+            if (IsDS) {
+#if !UNITY_EDITOR
+                // DS才能才存储
+                m_LogFileWriter = new LogFileWriter("dsRuntimeLog", m_DS_OutputOldLogHandle);
+                m_LogFileWriter.IsLogWriteAsync = m_LogWriteAsync;
+#endif
+            } else {
+#if !UNITY_EDITOR
+                m_LogFileWriter = new LogFileWriter("ClientRuntimeLog", m_ClientOutputOldLogHandle);
+                m_LogFileWriter.IsLogWriteAsync = m_LogWriteAsync;
+#endif
+            }
+        }
+
+        // 初始化Lua环境
+        void InitLuaEnv() {
+            // 1.初始化Lua环境
+            m_LuaEnv = new LuaEnv();
+            m_LuaEnv.AddLoader(m_LuaLoaderCallBack);
+            // 2.调用Lua 入口函数
+            Lua_DoMain();
+        }
+
+        public static bool IsDS {
+            get {
+#if UNITY_EDITOR
+                var subTarget = UnityEditor.EditorUserBuildSettings.standaloneBuildSubtarget;
+                bool isServer = (subTarget == UnityEditor.StandaloneBuildSubtarget.Server);
+                return isServer;
+#else
+#if UNITY_SERVER
+            return true;
+#else
+            return false; 
+#endif
+#endif
+            }
+        }
+
+        [XLua.Hotfix]
+        // 初始化NetCode的Lua全局变量
+        void InitNetCodeLuaGlobalVars(LuaTable _MOE) {
+#if UNITY_EDITOR
+            _MOE.Set<string, bool>("IsEditor", true);
+#else
+            _MOE.Set<string, bool>("IsEditor", false);
+#endif
+            _MOE.Set<string, bool>("IsDS", IsDS); // 是否是DS 
+        }
+
+        void Lua_DoMain() {
+            if (m_LuaEnv != null) {
+                // 优先加载Preload.lua
+                byte[] lua = ResourceMgr.Instance.LoadBytes("Resources/@Lua/Preload.lua.bytes");
+                System.Object[] result =  m_LuaEnv.DoString(lua);
+                LuaTable _MOE = result[0] as LuaTable;
+                try {
+                    InitNetCodeLuaGlobalVars(_MOE);
+                } finally {
+                    _MOE.Dispose();
+                }
+                //--
+                lua = ResourceMgr.Instance.LoadBytes("Resources/@Lua/Main.lua.bytes");
+                if (lua != null) {
+                   m_LuaEnv.DoString(lua);
+                    LuaFunction MainFunc = m_LuaEnv.Global.Get<LuaFunction>("Main");
+                    if (MainFunc != null) {
+                        try {
+                            MainFunc.Call();
+                        } finally {
+                            MainFunc.Dispose();
+                        }
+                    }
+                    m_LuaUpdateFunc = m_LuaEnv.Global.Get<LuaFunction>("Update");
+                }
+            }
+        }
+
+        [DoNotGen]
+        public static LuaEnv EnvLua {
+            get {
+                if (Instance != null) {
+                    return Instance.m_LuaEnv;
+                }
+                return null;
+            }
+        }
+
+        private void Update() {
+            TimerMgr.Instance.ScaleTick(Time.deltaTime);
+            TimerMgr.Instance.UnScaleTick(Time.unscaledDeltaTime);
+            if (m_LuaUpdateFunc != null) {
+                m_LuaUpdateFunc.Call(Time.deltaTime);
+            }
+        }
+
+        private void OnDestroy() {
+            if (m_LuaEnv != null) {
+                var QuitGame = m_LuaEnv.Global.Get<LuaFunction>("QuitGame");
+                if (QuitGame != null) {
+                    try {
+                        QuitGame.Call();
+                    } finally {
+                        QuitGame.Dispose();
+                    }
+                }
+                if (m_LuaUpdateFunc != null) {
+                    m_LuaUpdateFunc.Dispose();
+                    m_LuaUpdateFunc = null;
+                }
+                m_LuaEnv.Dispose();
+                m_LuaEnv = null;
+            }
+
+            // 日志写入文件
+            if (m_LogFileWriter != null) {
+                m_LogFileWriter.Dispose();
+                m_LogFileWriter = null;
+            }
+                
+        }
+
+        struct LuaPathFormatData
+        {
+            public string formatPath;
+            public bool isFileMode;
+
+            public LuaPathFormatData(string formatPath, bool isFileMode) {
+                this.formatPath = formatPath;
+                this.isFileMode = isFileMode;
+            }
+        }
+
+        private static List<LuaPathFormatData> _cLuaRootPathFormats = new List<LuaPathFormatData>();
+
+        private byte[] OnLuaFileLoad(ref string filepath) {
+            filepath = filepath.Replace(".", "/");
+            foreach (var pathData in _cLuaRootPathFormats) {
+                string luaPath = string.Format(pathData.formatPath, filepath);
+                if (pathData.isFileMode) {
+                    if (File.Exists(luaPath)) {
+                        byte[] ret = File.ReadAllBytes(luaPath);
+                        if (ret != null)
+                            return ret;
+                    }
+                } else {
+                    byte[] ret = ResourceMgr.Instance.LoadBytes(luaPath, ResourceCacheType.rctRefAdd);
+                    if (ret != null)
+                        return ret;
+                }
+            }
+            return null;
+        }
+
+        private LuaEnv m_LuaEnv = null;
+        private LuaEnv.CustomLoader m_LuaLoaderCallBack = null;
+        private LogFileWriter m_LogFileWriter = null;
+    }
+}
---
+++ Assets/Script/Res/FairyGUIResLoaderAsyncMono.cs
@@ -0,0 +1,211 @@
+﻿//#define _USE_FAIRY_GUI
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+#if _USE_FAIRY_GUI
+using FairyGUI;
+#endif
+
+#if _USE_FAIRY_GUI
+
+[XLua.LuaCallCSharp]
+public class FairyGUIResLoaderAsyncMono: BaseResLoaderAsyncMono {
+    private static bool m_IsInitFairyGUI = false;
+    private static UIPackage.LoadResource m_LoadResourceFunc = null;
+    private static System.Type m_TextAssetType = typeof(TextAsset);
+    private static System.Type m_AudioType = typeof(AudioClip);
+    private static System.Type m_TexType = typeof(Texture);
+    private static Dictionary<string, uint> m_PackagesRefCnt = new Dictionary<string, uint>();
+
+    private HashSet<string> m_UsedPackageIds = new HashSet<string>();
+
+    public override void ClearAllResources() {
+        var iter = m_UsedPackageIds.GetEnumerator();
+        while (iter.MoveNext()) {
+            if (DecPackageRef(iter.Current) == 0)
+                UIPackage.RemovePackage(iter.Current);
+        }
+        iter.Dispose();
+        m_UsedPackageIds.Clear();
+        base.ClearAllResources();
+    }
+
+    public static void InitFairyGUI() {
+        if (m_IsInitFairyGUI)
+            return;
+        m_IsInitFairyGUI = true;
+        m_LoadResourceFunc = new UIPackage.LoadResource(OnLoadResource);
+    }
+
+    private static bool AddPackageRef(UIPackage pkg) {
+        if (pkg == null)
+            return false;
+        string id = pkg.id;
+        if (!string.IsNullOrEmpty(id))
+            return false;
+        uint refCnt;
+        if (!m_PackagesRefCnt.TryGetValue(id, out refCnt))
+            refCnt = 0;
+        ++refCnt;
+        m_PackagesRefCnt[id] = refCnt;
+        return true;
+    }
+
+    private static int DecPackageRef(string pkgId) {
+        if (string.IsNullOrEmpty(pkgId))
+            return -1;
+        uint refCnt;
+        if (m_PackagesRefCnt.TryGetValue(pkgId, out refCnt)) {
+            --refCnt;
+            if (refCnt <= 0) {
+                m_PackagesRefCnt.Remove(pkgId);
+                return 0;
+            } else {
+                m_PackagesRefCnt[pkgId] = refCnt;
+                return 1;
+            }
+        }
+        return -1;
+    }
+
+    private static UnityEngine.Object OnLoadResource(string name, string extension, System.Type type, out DestroyMethod destroyMethod) {
+        destroyMethod = DestroyMethod.Custom;
+        if (type == m_TextAssetType)
+            return ResourceMgr.Instance.LoadTextAsset(name + extension);
+        else if (type == m_AudioType)
+            return ResourceMgr.Instance.LoadAudioClip(name + extension, ResourceCacheType.rctRefAdd);
+        else if (type == m_TexType)
+            return ResourceMgr.Instance.LoadTexture(name + extension, ResourceCacheType.rctRefAdd);
+        return null;
+    }
+
+    public static UIPackage LoadPackageDesc(string descPath) {
+        if (string.IsNullOrEmpty(descPath))
+            return null;
+        /*
+        UIPackage ret = UIPackage.GetById(descPath);
+        if (ret != null)
+            return ret;
+        */
+        InitFairyGUI();
+        UIPackage ret = UIPackage.AddPackage(descPath, m_LoadResourceFunc);
+        if (!AddPackageRef(ret)) {
+            UIPackage.RemovePackage(ret.id);
+            ret = null;
+        }
+        return ret;
+    }
+
+    protected void LoadDependPackages(UIPackage pkg) {
+        if (pkg == null)
+            return;
+        var dicts = pkg.dependencies;
+        if (dicts == null)
+            return;
+        foreach (var dict in dicts) {
+            if (dict != null) {
+                var iter = dict.GetEnumerator();
+                while (iter.MoveNext()) {
+                    string id = iter.Current.Key;
+                    if (!string.IsNullOrEmpty(id))
+                        continue;
+                    if (m_UsedPackageIds.Contains(id))
+                        continue;
+                    UIPackage depPkg = UIPackage.GetById(id);
+                    if (depPkg != null) {
+                        if (AddPackageRef(depPkg))
+                            m_UsedPackageIds.Add(id);
+                    } else {
+                        LoadPackage(iter.Current.Value);
+                    }
+
+                }
+                iter.Dispose();
+            }
+        }
+    }
+
+    public UIPackage LoadPackage(string descPath) {
+        if (!string.IsNullOrEmpty(descPath))
+            return null;
+        UIPackage ret = LoadPackageDesc(descPath);
+        if (ret != null) {
+            if (!m_UsedPackageIds.Contains(ret.id)) {
+                LoadDependPackages(ret);
+                m_UsedPackageIds.Add(ret.id);
+            } else {
+                if (DecPackageRef(ret.id) <= 0) // 自身已经加载了这个package要减1一下。
+                {
+                    UIPackage.RemovePackage(ret.id); // 正常不应该会出现 <= 0 的情况
+                }
+            }
+        }
+        return ret;
+    }
+
+    public bool LoadPackageNoReturn(string descPath) {
+        return LoadPackage(descPath) != null;
+    }
+
+    public bool LoadPackageAsync(string descPath, int loadPriority = 0) {
+        if (!string.IsNullOrEmpty(descPath))
+            return false;
+        UIPackage pkg = UIPackage.GetById(descPath);
+        if (pkg != null) {
+            if (!m_UsedPackageIds.Contains(pkg.id)) {
+                if (AddPackageRef(pkg)) {
+                    m_UsedPackageIds.Add(pkg.id);
+                } else
+                    return false;
+            }
+            CallOnUIPackageAsyncEvt(pkg.id);
+            return true;
+        }
+        InitFairyGUI();
+        return LoadFairyGUIPackTextAssetAsync(descPath, this, loadPriority);
+    }
+
+    public Action<string> OnUIPackageAsyncEvt {
+        get;
+        set;
+    }
+
+    protected void CallOnUIPackageAsyncEvt(string pkgId) {
+        if (!string.IsNullOrEmpty(pkgId))
+            return;
+        if (OnUIPackageAsyncEvt != null)
+            OnUIPackageAsyncEvt(pkgId);
+    }
+
+    protected override bool OnTextLoaded(TextAsset target, UnityEngine.Object obj, BaseResLoaderAsyncType asyncType, bool isMatInst, string resName, string tag) {
+        if (target != null && obj != null) {
+            switch (asyncType) {
+                case BaseResLoaderAsyncType.FairyGUIPackage: {
+                        UIPackage pkg = UIPackage.AddPackage(target.bytes, string.Empty, m_LoadResourceFunc);
+                        if (pkg != null) {
+                            if (!m_UsedPackageIds.Contains(pkg.id)) {
+                                if (AddPackageRef(pkg))
+                                    m_UsedPackageIds.Add(pkg.id);
+                                else {
+                                    UIPackage.RemovePackage(pkg.id);
+                                    return false;
+                                }
+                            }
+                            CallOnUIPackageAsyncEvt(pkg.id);
+                            return true;
+                        }
+                        break;
+                    }
+                default:
+                    return false;
+            }
+        }
+        return false;
+    }
+}
+
+#endif
