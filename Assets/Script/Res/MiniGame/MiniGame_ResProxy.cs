8a7aabbc61abab07f8e29fc4a53e7a5b4952314f
billwillman <billwillman@qq.com>
2025-01-01 01:49:19 +0800

    111

--- Assets/Script/Res/MiniGame/MiniGame_ResProxy.cs
+++ Assets/Script/Res/MiniGame/MiniGame_ResProxy.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+#if UNITY_WEIXINMINIGAME
+using WeChatWASM;
+#endif
 // 文件代理管理
 public class MiniGame_ResProxyMgr: SingetonMono<MiniGame_ResProxyMgr> 
 {
