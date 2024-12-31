909117cf9feac7da728a604b0c8a7c945551b8b2
billwillman <billwillman@qq.com>
2025-01-01 01:31:23 +0800

    222

---
+++ Assets/Script/Res/MiniGame_ResProxy.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 文件代理管理
+public class MiniGame_ResProxyMgr: SingetonMono<MiniGame_ResProxyMgr> 
+{
+    public string CDNRoot = string.Empty;
+}
