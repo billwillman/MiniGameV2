b1df755353561333e9e4ebb96cbf24b00a81ec39
billwillman <billwillman@qq.com>
2025-01-08 00:27:32 +0800

    1111

--- Assets/Script/tcp/WX/WXTcpClient.cs
+++ Assets/Script/tcp/WX/WXTcpClient.cs
@@ -1,11 +1,48 @@
+#if UNITY_WEIXINMINIGAME
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WeChatWASM;
 
 namespace NsTcpClient
 {
-    public class WXTcpClient
+    public class WXTcpClient: ITcpClient
     {
+        private WXTCPSocket m_TcpSocket = null; // н╒пе Tcp Socket
+
+        public Action<TcpClient> OnThreadBufferProcess { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public bool Connect(string pRemoteIp, int uRemotePort, int mTimeOut = -1) {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose() {
+            throw new NotImplementedException();
+        }
+
+        public int GetReadDataNoLock(byte[] pBuffer, int offset) {
+            throw new NotImplementedException();
+        }
+
+        public eClientState GetState() {
+            throw new NotImplementedException();
+        }
+
+        public bool HasReadData() {
+            throw new NotImplementedException();
+        }
+
+        public void Release() {
+            throw new NotImplementedException();
+        }
+
+        public bool Send(byte[] pData, int bufSize = -1) {
+            throw new NotImplementedException();
+        }
     }
 
 }
+
+#endif
