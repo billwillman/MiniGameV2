b7da2e9cb02da804c81760ea4832655b257f73ee
billwillman <billwillman@qq.com>
2025-01-08 00:45:52 +0800

    33333

--- Assets/Script/tcp/WX/WXTcpClient.cs
+++ Assets/Script/tcp/WX/WXTcpClient.cs
@@ -11,35 +11,58 @@ namespace NsTcpClient
     public class WXTcpClient: ITcpClient
     {
         private WXTCPSocket m_TcpSocket = null; // 微信 Tcp Socket
+        private bool m_IsDispose = false;
+        private eClientState m_State = eClientState.eCLIENT_STATE_NONE;
+        private int m_HasReadSize = 0;
 
         public WXTcpClient() {
             m_TcpSocket = WX.CreateTCPSocket();
         }
 
-        public Action<TcpClient> OnThreadBufferProcess { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        ~WXTcpClient() {
+            Dispose(false);
+        }
+
+        public eClientState GetState() {
+            return m_State;
+        }
+
+        public Action<TcpClient> OnThreadBufferProcess {
+            get;
+            set;
+        }
 
         public bool Connect(string pRemoteIp, int uRemotePort, int mTimeOut = -1) {
             throw new NotImplementedException();
         }
 
         public void Dispose() {
-            throw new NotImplementedException();
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool Diposing) {
+            if (!m_IsDispose) {
+
+                if (Diposing) {
+                    m_HasReadSize = 0;
+                }
+
+                // 释放非托管对象资源
+                m_IsDispose = true;
+            }
         }
 
         public int GetReadDataNoLock(byte[] pBuffer, int offset) {
             throw new NotImplementedException();
         }
 
-        public eClientState GetState() {
-            throw new NotImplementedException();
-        }
-
         public bool HasReadData() {
-            throw new NotImplementedException();
+            return (m_HasReadSize > 0);
         }
 
         public void Release() {
-            throw new NotImplementedException();
+            Dispose();
         }
 
         public bool Send(byte[] pData, int bufSize = -1) {
