297ba34d589c34295eb3542a76e327474c8000cf
billwillman <billwillman@qq.com>
2025-01-12 04:52:27 +0800

    1111111111

--- Assets/Script/GamePlay/Dots/CustomEntiyManager.cs
+++ Assets/Script/GamePlay/Dots/CustomEntiyManager.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Jobs;
 
 namespace SOC.GamePlay
 {
 
+    public class TestRotJob : IJobParallelForTransform
+    {
+        public void Execute(int index, TransformAccess transform) {
+          
+        }
+    }
+
     public class CustomEntiyManager : ILuaBinder
     {
         public GameObject m_InstanceObject = null;
+        public override void OnUpdate() {
+            
+        }
     }
 
 }
