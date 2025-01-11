a92f67c3ce4f973de4e262f0fcebc96059573dbc
billwillman <billwillman@qq.com>
2025-01-12 04:55:16 +0800

    22222

--- Assets/Script/GamePlay/Dots/CustomEntiyManager.cs
+++ Assets/Script/GamePlay/Dots/CustomEntiyManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Jobs;
+using Unity.Burst;
 
 namespace SOC.GamePlay
 {
 
+    [BurstCompile]
     public class TestRotJob : IJobParallelForTransform
     {
+        [BurstCompile]
         public void Execute(int index, TransformAccess transform) {
           
         }
