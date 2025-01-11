502f67afa641e70e11137508bd4db2405dbdef17
bestzeng <bestzeng@tencent.com>
2025-01-11 23:41:48 +0800

    22222

---
+++ Assets/Script/GamePlay/Dots/CustomEntiyManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SOC.GamePlay
+{
+
+    public class CustomEntiyManager : ILuaBinder
+    {
+        public GameObject m_InstanceObject = null;
+    }
+
+}
