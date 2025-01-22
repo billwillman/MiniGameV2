d9fde399c413d260e75fab9f2280358eb3cd3328
billwillman <billwillman@qq.com>
2025-01-23 02:10:49 +0800

    2222111

--- Assets/Script/Skill/AnimancerTrackAsset.cs
+++ Assets/Script/Skill/AnimancerTrackAsset.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using Animancer;
 
 [System.Serializable]
 public class AnimancerTrackAsset : PlayableAsset
 {
+    public ClipTransition m_AnimationClip = null;
     // Factory method that generates a playable based on this asset
     public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
     {
