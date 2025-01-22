bf261ba3b56477bb5e26be4329b40b7fc8340691
billwillman <billwillman@qq.com>
2025-01-23 02:59:42 +0800

    2222

--- Assets/Script/Skill/AnimancerBehaviour.cs
+++ Assets/Script/Skill/AnimancerBehaviour.cs
@@ -9,30 +9,30 @@ public class AnimancerBehaviour : PlayableBehaviour
     // Called when the owning graph starts playing
     public override void OnGraphStart(Playable playable)
     {
-        
+        base.OnGraphStart(playable);
     }
 
     // Called when the owning graph stops playing
     public override void OnGraphStop(Playable playable)
     {
-        
+        base.OnGraphStop(playable);
     }
 
     // Called when the state of the playable is set to Play
     public override void OnBehaviourPlay(Playable playable, FrameData info)
     {
-        
+        base.OnBehaviourPlay(playable, info);
     }
 
     // Called when the state of the playable is set to Paused
     public override void OnBehaviourPause(Playable playable, FrameData info)
     {
-        
+        base.OnBehaviourPause(playable, info);
     }
 
     // Called each frame while the state is set to Play
     public override void PrepareFrame(Playable playable, FrameData info)
     {
-        
+        base.PrepareFrame(playable, info);
     }
 }
