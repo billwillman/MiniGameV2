a3c6e4528b03f63366bf9087a532dc3ff2ee8e65
billwillman <billwillman@qq.com>
2025-01-24 00:37:22 +0800

    1111

--- Assets/Script/Skill/AnimancerTimeline.cs
+++ Assets/Script/Skill/AnimancerTimeline.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 using Animancer;
 
 [RequireComponent(typeof(AnimancerComponent))]
@@ -20,10 +21,14 @@ public class AnimancerTimeline : MonoBehaviour
     }
 
     private void Awake() {
+        var director = GetComponent<PlayableDirector>();
+        if (director != null)
+            director.enabled = false;
         if (m_PlayOnAwake && m_CurrentTimeline != null) {
             var playGraph = this.Playable;
             if (playGraph != null) {
                 playGraph.Play(m_CurrentTimeline);
+               // m_CurrentTimeline.CreateStateAndApply(this.Playable.Playable).
             }
         }
     }
