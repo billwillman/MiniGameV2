047b795f90983cb217c9c5810cbf4f6a667ac7e5
billwillman <billwillman@qq.com>
2025-01-26 14:30:50 +0800

    2312321

--- Assets/Script/Skill/SkillManager.cs
+++ Assets/Script/Skill/SkillManager.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Animancer;
 
 namespace SOC.GamePlay
 {
     public class SkillManager : ILuaBinder
     {
-        
+        private AnimancerComponent m_AnimancerComponent = null;
+        public AnimancerComponent Animancer {
+            get {
+                if (m_AnimancerComponent == null)
+                    m_AnimancerComponent = GetComponentInChildren<AnimancerComponent>();
+                return m_AnimancerComponent;
+            }
+        }
     }
 }
