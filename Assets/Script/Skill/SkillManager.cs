78b4ca0f81bdf83f2b57ebf3d29b0686c7db2c45
billwillman <billwillman@qq.com>
2025-01-26 17:15:06 +0800

    1111

--- Assets/Script/Skill/SkillManager.cs
+++ Assets/Script/Skill/SkillManager.cs
@@ -9,6 +9,12 @@ namespace SOC.GamePlay
     public class SkillManager : ILuaBinder
     {
         private AnimancerComponent m_AnimancerComponent = null;
+
+        /// <summary>
+        /// 注册的技能类（对应Lua），一个技能支持多个Layer,一个Layer支持多个State(每个Layer可以State独立)
+        /// </summary>
+        public string[] RegisterSkillClasses = null;
+
         public AnimancerComponent Animancer {
             get {
                 if (m_AnimancerComponent == null)
