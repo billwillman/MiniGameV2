be87fe57b054357fe72e1d8c660d64de079d0caa
billwillman <billwillman@qq.com>
2025-01-29 15:25:11 +0800

    222

---
+++ Assets/Script/GamePlay/TimelineLuaReceiver.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+namespace SOC.GamePlay
+{
+    [RequireComponent(typeof(ILuaBinder))]
+    public class TimelineEventLuaReceiver : SignalReceiver
+    {
+
+    }
+
+}
