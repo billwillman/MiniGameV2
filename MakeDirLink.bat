2b1e2ac273e3af927b294b0583fa16a8254587bf
billwillman <billwillman@qq.com>
2025-02-01 02:34:29 +0800

    2222

--- MakeDirLink.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo CurrentDir: %~dp0
-
-mklink /d "%~dp0/Library/PackageCache/com.unity.netcode.gameobjects" "%~dp0/backup/com.unity.netcode.gameobjects"
-mklink /d "%~dp0/outPath/Proj/Library/PackageCache/com.unity.netcode.gameobjects" "%~dp0/backup/com.unity.netcode.gameobjects"
-
-pause
