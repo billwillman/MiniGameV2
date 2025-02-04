150f4589d8f3941a2dccd18857255f3904cc0b4a
billwillman <billwillman@qq.com>
2025-02-04 20:57:35 +0800

    2222

--- Server/Lua/DSA/DSAMsgProcesser.lua
+++ Server/Lua/DSA/DSAMsgProcesser.lua
@@ -12,6 +12,17 @@ require("ServerCommon.GlobalFuncs")
 
 --- 客户端发送给服务器请求协议处理
 local ClientToServerMsgProcess = {
+    [MsgIds.CM_DS_Ready] = function (self, msg, socket, fd)
+        local ip, port = GetIpAndPort(socket, fd)
+        local token = GenerateToken2(ip, port)
+        local ds = _MOE.DSMap[token]
+        if ds and ds.dsData then
+            ds.dsData.isLocalDS = msg.isLocalDS -- 是否是Local DS
+            print(string.format("[DSA] token: %s isLocalDS: %s", token, tostring(msg.isLocalDS)))
+            return true
+        end
+        return false
+    end
 }
 -----------------------------
 
--- Server/Lua/DSA/DSAServer.lua
+++ Server/Lua/DSA/DSAServer.lua
@@ -15,6 +15,20 @@ moon.exports.ServerData = ServerData
 moon.exports.OnAccept = function(fd, msg)
     print("accept ", fd, moon.decode(msg, "Z"))
     socket.settimeout(fd, 10)
+
+    ---------------- DS数据存储 --------------------------
+    _MOE.DSMap = _MOE.DSMap or {}
+    local ip, port = GetIpAndPort(socket, fd)
+    local token = GenerateToken2(ip, port)
+    _MOE.DSMap[token] = {
+        fd = fd,
+        dsData = {
+            ip = ip,
+            port = msg.port
+        }
+    }
+    print(string.format("[DSA] Accept DS => token: %s ip: %s port: %d dsPort: %d", token, ip, port, msg.port))
+    -----------------------------------------------------
 end
 
 moon.exports.OnMessage = function(fd, msg)
@@ -23,4 +37,11 @@ end
 
 moon.exports.OnClose = function(fd, msg)
     print("close ", fd, moon.decode(msg, "Z"))
+
+    if _MOE.DSMap then
+        local ip, port = GetIpAndPort(socket, fd)
+        local token = GenerateToken2(ip, port)
+        _MOE.DSMap[token] = nil
+        print(string.format("[DSA] Close DS => token: %s ip: %s port: %d", token, ip, port))
+    end
 end
