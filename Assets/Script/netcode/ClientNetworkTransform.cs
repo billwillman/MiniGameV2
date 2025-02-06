ce75210a593371f9351fc7f69aa2fc723c936aac
billwillman <billwillman@qq.com>
2025-02-07 01:43:01 +0800

    1111

---
+++ Assets/Script/netcode/ClientNetworkTransform.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode.Components
+
+namespace SOC.GamePlay
+{
+
+    public class ClientNetworkTransform : NetworkTransform
+    {
+        protected override bool OnIsServerAuthoritative()
+        {
+            return false;
+        }
+    }
+}
