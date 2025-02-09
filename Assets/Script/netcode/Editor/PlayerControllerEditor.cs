339c5e91b8f9bed4e0e8d580fa1609e6bc92c1bc
billwillman <billwillman@qq.com>
2025-02-09 15:06:17 +0800

    2222

---
+++ Assets/Script/netcode/Editor/PlayerControllerEditor.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using Unity.Netcode;
+
+namespace SOC.GamePlay
+{
+    [CustomEditor(typeof(PlayerController))]
+    public class PlayerControllerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            if (EditorApplication.isPlaying)
+            {
+                PlayerController obj = target as PlayerController;
+                if (obj != null)
+                {
+                    if (obj.PawnId.CanClientRead(obj.OwnerClientId))
+                    {
+                        EditorGUILayout.LongField("[PawnId]", (long)obj.PawnId.Value);
+                    }
+                    if (obj.Pawn != null && obj.Pawn.IsSpawned)
+                    {
+                        EditorGUILayout.ObjectField("[Pawn]", obj.Pawn, typeof(NetworkObject), true);
+                    }
+                }
+            }
+        }
+    }
+}
