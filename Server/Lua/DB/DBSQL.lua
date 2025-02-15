64347333398d9b514654affb9e275c8d7a29c9e3
billwillman <billwillman@qq.com>
2025-02-15 19:25:11 +0800

    2222

--- Server/Lua/DB/DBSQL.lua
+++ Server/Lua/DB/DBSQL.lua
@@ -3,6 +3,7 @@ local M = {}
 -- pgsql port 5432
 -- uuid字段 自动生成uuid表达式： gen_random_uuid()
 
+----- 修改sha256登录加密方式更改为md5模式，moon只支持md5模式
 -- SHOW password_encryption;
 -- SET password_encryption='md5';
 -- ALTER USER postgres with password 'GameBryo1122';
