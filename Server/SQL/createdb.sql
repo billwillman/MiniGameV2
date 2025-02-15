5c47f465cb72ddbaec5479e10de1895e8f752288
billwillman <billwillman@qq.com>
2025-02-15 16:43:32 +0800

    1111

---
+++ Server/SQL/createdb.sql
@@ -0,0 +1,14 @@
+-- Database: minigame
+
+-- DROP DATABASE IF EXISTS minigame;
+
+CREATE DATABASE minigame
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'zh-CN'
+    LC_CTYPE = 'zh-CN'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
---
+++ Server/SQL/user.sql
@@ -0,0 +1,18 @@
+-- Table: public.user
+
+-- DROP TABLE IF EXISTS public."user";
+
+CREATE TABLE IF NOT EXISTS public."user"
+(
+    id uuid NOT NULL DEFAULT gen_random_uuid(),
+    username character varying COLLATE pg_catalog."default" NOT NULL,
+    password character varying COLLATE pg_catalog."default" DEFAULT ''::character varying,
+    "isLock" bit(1) NOT NULL DEFAULT (1)::bit(1),
+    "lockEndTime" time without time zone,
+    CONSTRAINT user_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."user"
+    OWNER to postgres;
