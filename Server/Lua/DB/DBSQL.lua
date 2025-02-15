7977353d21520e8cd87251eaab353ed9c1eeb0d2
billwillman <billwillman@qq.com>
2025-02-15 18:42:09 +0800

    222

--- Server/Lua/DB/DBSQL.lua
+++ Server/Lua/DB/DBSQL.lua
@@ -2,6 +2,9 @@ local M = {}
 
 -- pgsql port 5432
 -- uuid gen_random_uuid()
+
+-- SHOW password_encryption;
 -- SET password_encryption='md5';
+-- ALTER USER postgres with password 'GameBryo1122';
 
 return M
