35c9b609b6f45ea614b7bf197ec4051ebe924930
billwillman <billwillman@qq.com>
2025-02-15 19:44:30 +0800

    2222

--- Server/Lua/ServerCommon/ServerMsgIds.lua
+++ Server/Lua/ServerCommon/ServerMsgIds.lua
@@ -7,6 +7,7 @@ _MOE.ServerMsgIds = {
     SM_LS_Exist_PLAYERINFO = "SM_LS_Exist_PLAYERINFO", -- 根据LS的Token查询是否存在PlayerInfo
     SM_LS_DS_Enter = "SM_LS_DS_Enter",  -- 通过LoginSrv通知Client连接DS
     SM_DSA_Exist_DS = "SM_DSA_Exist_DS",
+    SM_Login_Ret = "SM_Login_Ret",
 }
 
 _MOE.DsStatus = {
