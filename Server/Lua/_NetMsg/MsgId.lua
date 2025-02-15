ee12d4068f1d4fbbbf100def99bf9d2fd5a61edb
billwillman <billwillman@qq.com>
2025-02-15 19:28:36 +0800

    11111

--- Server/Lua/_NetMsg/MsgId.lua
+++ Server/Lua/_NetMsg/MsgId.lua
@@ -2,6 +2,7 @@ local MsgIds = {
     CM_Heart = 1, -- 心跳包(通用)
     CM_DS_Ready = 2, -- DS准备好了
     CM_ReqDS = 3, -- 请求DS
+    CM_Login = 4, -- 登录
 ---------------------------------------------------------
     SM_Heart = 1,
     SM_DS_Info = 2,
