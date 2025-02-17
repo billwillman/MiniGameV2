e3b977675879247a801c59cb049900b472748ff0
billwillman <billwillman@qq.com>
2025-02-18 02:52:51 +0800

    1111

---
+++ LocalDS_Win.bat
@@ -0,0 +1,4 @@
+@echo off
+echo CurrentDir: %~dp0
+cd "%~dp0/outPath/DS/"
+Server.exe "{'dsData':{'ip':'127.0.0.1','port':7777, 'scene': 'MultiScene'},'GsData':{'ip':'127.0.0.1','port':1991}, 'isLocalDS':true}"
