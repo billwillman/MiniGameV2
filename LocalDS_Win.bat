da5ddda8293cd161d36403a7608e9f52c4883631
billwillman <billwillman@qq.com>
2025-02-20 01:43:06 +0800

    2222

--- LocalDS_Win.bat
+++ LocalDS_Win.bat
@@ -1,7 +1,7 @@
 @echo off
 echo CurrentDir: %~dp0
 cd "%~dp0/outPath/DS/"
-set json={"dsData":{"ip":"127.0.0.1","port":7777,"scene":"MultiScene"},"GsData":{"ip":"127.0.0.1","port":1991},"isLocalDS":true}
-Server.exe "%json%"
+set json='{\"dsData\":{\"ip\":\"127.0.0.1\",\"port\":7777,\"scene\":\"MultiScene\"},\"GsData\":{\"ip\":\"127.0.0.1\",\"port\":1991},\"isLocalDS\":true}'
+Server.exe %json%
 cd ..
 cd ..
