86fac3a845e5458a13a83e9b12473c771dfad2ca
billwillman <billwillman@qq.com>
2025-02-22 03:57:49 +0800

    22222

--- Server/Lib/moon/newbuild.sh
+++ Server/Lib/moon/newbuild.sh
@@ -1,1 +1,13 @@
-make config=release -j4
+#!/bin/bash
+
+git submodule init
+git submodule update
+git pull
+
+# Detect the operating system
+os=$(uname)
+
+# Execute Linux commands
+echo "Running on Linux"
+cpu_num=$(cat /proc/cpuinfo | grep "processor" | wc -l)
+make config=release -j$cpu_num
