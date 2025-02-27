b21f2abb64e9853ba2283034cd7e4f646f1d9dc7
billwillman <billwillman@qq.com>
2025-02-28 02:53:17 +0800

    22222

---
+++ Assets/Script/tcp/NetUtils.cs
@@ -0,0 +1,29 @@
+#if UNITY_WEIXINMINIGAME
+#else
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+
+public static class NetUtils
+{
+
+    // 返回 0 表示无效的地址
+    public static int GetFreePort_TcpV4()
+    {
+        try
+        {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            TcpListener listener = new TcpListener(ipAddress, 0);
+            listener.Start();
+            int port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
+            return port;
+        } catch
+        {
+            return 0;
+        }
+    }
+}
+
+#endif
