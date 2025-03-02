6c3cd7c6707b90fc06981c475d36e2684e48ab3d
billwillman <billwillman@qq.com>
2025-03-03 00:58:33 +0800

    2222

--- Assets/Resources/@Lua/_Client/ClientPlayerControllerContainer.lua
+++ Assets/Resources/@Lua/_Client/ClientPlayerControllerContainer.lua
@@ -3,4 +3,21 @@ local _M = _MOE.class("ClientPlayerControllerContainer", BaseClass)
 
 _M[".superctor"] = true -- 会调用父类方法
 
+require("_Common.Net.DSMsgDefine")
+
+-------------------------------- 外部调用 -------------------------------
+
+function _M:OnLoginDS()
+    local PlayerController = _MOE.GameApp:GetLocalPlayerController()
+    local userInfo = _MOE.Models.RootAttr:GetUserInfo()
+    if userInfo and userInfo.token then
+        PlayerController:DispatchServer_Reliable(_MOE.DS.ClientMsgIds.ClientLoginDS, userInfo.token) -- 发送Toekn给DS
+    end
+end
+
+function _M:OnLoginOutDS()
+end
+
+-------------------------------------------------------------------------
+
 return _M
---
+++ Assets/Resources/@Lua/_Common/Net/DSMsgDefine.lua
@@ -0,0 +1,9 @@
+------------ DS消息定义
+_MOE = _MOE or {}
+_MOE.DS = _MOE.DS or {}
+_MOE.DS.ClientMsgIds = {
+    ClientLoginDS = "ClientLoginDS",
+}
+
+_MOE.DS.DSMsgIds = {
+}
