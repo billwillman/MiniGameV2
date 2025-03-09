87dce3c060c7e7271f4c640a585854f3c4836131
billwillman <billwillman@qq.com>
2025-03-10 01:19:34 +0800

    1111

--- Server/Lua/LoginServer/SessionFreeTask.lua
+++ Server/Lua/LoginServer/SessionFreeTask.lua
@@ -16,7 +16,29 @@ function Task:Start()
     moon.async(TaskTick, self.__cname)
 end
 
-function FreeSessionTask:AddFreeSession(session)
+function Task:AddFreeSession(session)
+    if not session then
+        return false
+    end
+    local uuid = session:GetUUID()
+    if not uuid then
+        return false
+    end
+    local Player = self.SessionTempDataMap[uuid]
+    if Player then
+        --- 更新即可
+        Player.dsData = session.dsData
+        Player.freeTime = os.time()
+        ----
+        return true
+    end
+    Player = {
+        uid = uuid,
+        dsData = session.dsData,
+        freeTime = os.time()
+    }
+    self.SessionTempDataMap[uuid] = Player
+    _MOE.FreeSessionList:insert_last(Player)
 end
 
 return Task
