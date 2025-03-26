ed4f6d860ff813bb666d222db9fdb79017bbf0fe
billwillman <billwillman@qq.com>
2025-03-27 02:17:34 +0800

    22222

--- Assets/Script/GamePlay/MoeCharacterController.cs
+++ Assets/Script/GamePlay/MoeCharacterController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using KinematicCharacterController;
 using Animancer;
 using System.Collections.Generic;
+using System;
 
 namespace SOC.GamePlay
 {
@@ -33,41 +34,84 @@ namespace SOC.GamePlay
            
         }
 
+        public Func<Quaternion, float, Quaternion> onUpdateRotation
+        {
+            get;
+            set;
+        }
+
+        public Func<Vector3, float, Vector3> onUpdateVelocity
+        {
+            get;
+            set;
+        }
+
+        public Action<float> onBeforeCharacterUpdate
+        {
+            get;
+            set;
+        }
+
+        public Action<float> onPostGroundingUpdate
+        {
+            get;
+            set;
+        }
+
+        public Action<float> onAfterCharacterUpdate
+        {
+            get;
+            set;
+        }
+
+        public Func<Collider, bool> onIsColliderValidForCollisions
+        {
+            get;
+            set;
+        }
+
         // ------------------------------------------- Collision and Slide 移动控制相关 ---------------------------------------- //
         /// <summary>
         /// This is called when the motor wants to know what its rotation should be right now
         /// </summary>
         virtual public void UpdateRotation(ref Quaternion currentRotation, float deltaTime) {
-
+            if (onUpdateRotation != null)
+                currentRotation = onUpdateRotation(currentRotation, deltaTime);
         }
         /// <summary>
         /// This is called when the motor wants to know what its velocity should be right now
         /// </summary>
         virtual public void UpdateVelocity(ref Vector3 currentVelocity, float deltaTime) {
-
+            if (onUpdateVelocity != null)
+                currentVelocity = onUpdateVelocity(currentVelocity, deltaTime);
         }
         /// <summary>
         /// This is called before the motor does anything
         /// </summary>
         virtual public void BeforeCharacterUpdate(float deltaTime) {
-
+            if (onBeforeCharacterUpdate != null)
+                onBeforeCharacterUpdate(deltaTime);
         }
         /// <summary>
         /// This is called after the motor has finished its ground probing, but before PhysicsMover/Velocity/etc.... handling
         /// </summary>
         virtual public void PostGroundingUpdate(float deltaTime) {
-
+            if (onPostGroundingUpdate != null)
+                onPostGroundingUpdate(deltaTime);
         }
         /// <summary>
         /// This is called after the motor has finished everything in its update
         /// </summary>
         virtual public void AfterCharacterUpdate(float deltaTime) {
-
+            if (onAfterCharacterUpdate != null)
+                onAfterCharacterUpdate(deltaTime);
         }
         /// <summary>
         /// This is called after when the motor wants to know if the collider can be collided with (or if we just go through it)
         /// </summary>
         virtual public bool IsColliderValidForCollisions(Collider coll) {
+            if (onIsColliderValidForCollisions != null)
+                return onIsColliderValidForCollisions(coll);
             return true;
         }
         /// <summary>
