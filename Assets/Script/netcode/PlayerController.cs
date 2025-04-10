9267fa080b957de459f48d1ec56872130a3b0950
billwillman <billwillman@qq.com>
2025-04-11 00:23:36 +0800

    2222

--- Assets/Script/netcode/PlayerController.cs
+++ Assets/Script/netcode/PlayerController.cs
@@ -210,8 +210,8 @@ namespace SOC.GamePlay
         public NetworkVariable<ulong> PawnId = new NetworkVariable<ulong>(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
 
         // Int属性数组
-        [NonSerialized]
-        public NetworkVariable<NativeArray<int>> NetworkIntAttribute = new NetworkVariable<NativeArray<int>>(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server); 
+        //[NonSerialized]
+        //public NetworkVariable<NativeArray<int>> NetworkIntAttribute = new NetworkVariable<NativeArray<int>>(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server); 
 
         // 运行时的角色
         public NetworkObject Pawn
