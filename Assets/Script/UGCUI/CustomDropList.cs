267d7c73ab44e68cd9e3e4498896a7ba1ef7ac09
billwillman <billwillman@qq.com>
2025-05-04 18:35:13 +0800

    1111

--- Assets/Script/UGCUI/CustomDropList.cs
+++ Assets/Script/UGCUI/CustomDropList.cs
@@ -1,46 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.InputSystem;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem.UI;
+using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.Utilities;
+using UnityEngine.Serialization;
+using static UnityEditor.Progress;
 
-public class CustomDropList : Dropdown
+namespace UnityEngine.InputSystem.UI
 {
 
-    protected InputSystemUIInputModule NewUIInputModule
+    public class CustomDropList : Dropdown
     {
-        get
-        {
-            if (EventSystem.current == null)
-                return null;
-            var ret = EventSystem.current.currentInputModule as InputSystemUIInputModule;
-            return ret;
-        }
-    }
 
-    protected override void DestroyItem(DropdownItem item)
-    {
-        // 处理选择
-        var UIInputModule = this.NewUIInputModule;
-        if (UIInputModule != null)
+        protected static InputSystemUIInputModule NewUIInputModule
         {
-            
+            get
+            {
+                if (EventSystem.current == null)
+                    return null;
+                var ret = EventSystem.current.currentInputModule as InputSystemUIInputModule;
+                return ret;
+            }
         }
-        // -----------
-        base.DestroyItem(item);
-    }
 
-    protected override void DestroyDropdownList(GameObject dropdownList)
-    {
-        // 处理选择
-        var UIInputModule = this.NewUIInputModule;
-        if (UIInputModule != null)
+        private static void CheckRemoveObj(UnityEngine.Object target)
         {
-
+            if (target == null)
+                return;
+            var UIInputModule = NewUIInputModule;
+            if (UIInputModule != null)
+            {
+                /*
+                var t = typeof(InputSystemUIInputModule);
+                var flags = BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public;
+                var field = t.GetField("m_PointerStates", flags);
+                var items = field.GetValue(UIInputModule) as IEnumerable<PointerModel>;
+                */
+                UIInputModule.SendMessage("ResetPointers");
+            }
         }
-        // -----------
-        base.DestroyDropdownList(dropdownList);
+
+        protected override void DestroyItem(DropdownItem item)
+        {
+            // 处理选择
+           // CheckRemoveObj(item);
+            // -----------
+            base.DestroyItem(item);
+        }
+
+        protected override void DestroyDropdownList(GameObject dropdownList)
+        {
+            // 处理选择
+            CheckRemoveObj(dropdownList);
+            // -----------
+            base.DestroyDropdownList(dropdownList);
+        }
     }
 }
