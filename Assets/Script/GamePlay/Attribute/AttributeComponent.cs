281fcca0141e554222c712c088640667044be026
billwillman <billwillman@qq.com>
2025-05-07 02:29:26 +0800

    2222

--- Assets/Script/GamePlay/Attribute/AttributeComponent.cs
+++ Assets/Script/GamePlay/Attribute/AttributeComponent.cs
@@ -1,9 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using OD;
 using UnityEngine;
 
 namespace SOC.GamePlay.Attribute
 {
     public class AttributeComponent : MonoBehaviour
-    {}
+    {
+        // Êý¾ÝÃæ°å
+        public OrderedDictionary<string, AttributeGroup<int>> IntValueBoard = new OrderedDictionary<string, AttributeGroup<int>>();
+        public OrderedDictionary<string, AttributeGroup<float>> FloatValueBoard = new OrderedDictionary<string, AttributeGroup<float>>();
+        public OrderedDictionary<string, AttributeGroup<string>> StringValueBoard = new OrderedDictionary<string, AttributeGroup<string>>();
+        public OrderedDictionary<string, AttributeGroup<long>> LongValueBoard = new OrderedDictionary<string, AttributeGroup<long>>();
+        //---------
+    }
 }
