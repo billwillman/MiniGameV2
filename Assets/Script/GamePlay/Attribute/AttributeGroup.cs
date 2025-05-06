8d1e511b4d3bc8dec9959aa5587fce86d597732a
billwillman <billwillman@qq.com>
2025-05-07 01:37:10 +0800

    1111

--- Assets/Script/GamePlay/Attribute/AttributeGroup.cs
+++ Assets/Script/GamePlay/Attribute/AttributeGroup.cs
@@ -8,6 +8,6 @@ namespace SOC.GamePlay.Attribute
 
    public class AttributeGroup<T>
     {
-        public OrderedDictionary<string, T> AttributeMap = new OrderedDictionary<string, T>();
+        public OrderedDictionary<ushort, T> AttributeMap = new OrderedDictionary<ushort, T>();
     }
 }
