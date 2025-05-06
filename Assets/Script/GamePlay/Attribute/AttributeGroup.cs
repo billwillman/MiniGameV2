ac8a0a713900d1406e5d940be987fcbd6694f764
billwillman <billwillman@qq.com>
2025-05-07 01:11:26 +0800

    111

---
+++ Assets/Script/GamePlay/Attribute/AttributeGroup.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SOC.GamePlay.Attribute
+{
+    public struct Attribute<T>
+    {
+        public T Value;
+    }
+
+    public struct AttributeGroupMeta
+    {
+        public string Name; // Ãû×Ö
+    }
+}
