2d3da06002f937ae948f8e1add3c74cc30b74cbc
billwillman <billwillman@qq.com>
2025-05-07 01:46:08 +0800

    2222

--- Assets/Script/GamePlay/Attribute/AttributeGroup.cs
+++ Assets/Script/GamePlay/Attribute/AttributeGroup.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using OD;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace SOC.GamePlay.Attribute
 {
 
-   public class AttributeGroup<T>
+   public class AttributeGroup<T>: UserNetworkVariableSerialization<T>
     {
         public OrderedDictionary<ushort, T> AttributeMap = new OrderedDictionary<ushort, T>();
     }
