d431685e1289b82a31850a4551c5f9fbe8f9b4b7
billwillman <billwillman@qq.com>
2025-05-25 03:46:00 +0800

    22121

--- Assets/Script/GamePlay/Attribute/AttributeComponent.cs
+++ Assets/Script/GamePlay/Attribute/AttributeComponent.cs
@@ -187,6 +187,7 @@ namespace SOC.GamePlay.Attribute
                                 ushort currentKey = 0;
                                 foreach (var intIter in iter.Attributes)
                                 {
+                                    Group1.Value = new IntAttributeGroup();
                                     Group1.Value.AttributeMap.Add(currentKey++, intIter.IntDefaultValue);
                                 }
                                 Group1.SetDirty(false); // 默认值不认为是
@@ -204,6 +205,7 @@ namespace SOC.GamePlay.Attribute
                                 ushort currentKey = 0;
                                 foreach (var int64Iter in iter.Attributes)
                                 {
+                                    Group2.Value = new Int64AttributeGroup();
                                     Group2.Value.AttributeMap.Add(currentKey++, int64Iter.Int64DefaultValue);
                                 }
                                 Group2.SetDirty(false);
@@ -220,6 +222,7 @@ namespace SOC.GamePlay.Attribute
                                 ushort currentKey = 0;
                                 foreach (var stringIter in iter.Attributes)
                                 {
+                                    Group3.Value = new StringAttributeGroup();
                                     Group3.Value.AttributeMap.Add(currentKey++, stringIter.StringDefaultValue);
                                 }
                                 Group3.SetDirty(false);
