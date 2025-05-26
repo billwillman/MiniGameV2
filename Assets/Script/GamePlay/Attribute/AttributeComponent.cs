9cac05c936a4a380436a2709d3ec3061915f529d
bestzeng <bestzeng@tencent.com>
2025-05-26 11:27:34 +0800

    122133

--- Assets/Script/GamePlay/Attribute/AttributeComponent.cs
+++ Assets/Script/GamePlay/Attribute/AttributeComponent.cs
@@ -54,6 +54,8 @@ namespace SOC.GamePlay.Attribute
 
         public bool SetNetworkIntGroupVars(int index, ushort key, int value)
         {
+            if (!this.IsSpawned)
+                throw new Exception("[SetNetworkIntGroupVars] not IsSpawned");
             if (index < 0 || index >= GetNetworkIntGroupVarsNum())
                 return false;
             var item = NetworkIntGroupVars[index];
@@ -72,6 +74,8 @@ namespace SOC.GamePlay.Attribute
 
         public bool SetNetworkInt64GroupVars(int index, ushort key, long value)
         {
+            if (!this.IsSpawned)
+                throw new Exception("[SetNetworkInt64GroupVars] not IsSpawned");
             if (index < 0 || index >= GetNetworkInt64GroupVarsNum())
                 return false;
             var item = NetworkInt64GroupVars[index];
@@ -90,6 +94,8 @@ namespace SOC.GamePlay.Attribute
 
         public bool SetNetworkStringGroupVars(int index, ushort key, string value)
         {
+            if (!this.IsSpawned)
+                throw new Exception("[SetNetworkStringGroupVars] not IsSpawned");
             if (index < 0 || index >= GetNetworkStringGroupVarsNum())
                 return false;
             var item = NetworkStringGroupVars[index];
@@ -227,7 +233,7 @@ namespace SOC.GamePlay.Attribute
             {
                 if (OnPreInitAttributeEvent != null)
                     OnPreInitAttributeEvent();
-                Dictionary<string, INetworkAttributeGroup> NetworkAttributeGroupMap = null;
+                //Dictionary<string, INetworkAttributeGroup> NetworkAttributeGroupMap = null;
                 foreach (var iter in AttributeGroupMeta)
                 {
                     switch (iter.AttributeType)
@@ -237,14 +243,17 @@ namespace SOC.GamePlay.Attribute
                             {
                                 if (NetworkIntGroupVars == null)
                                     NetworkIntGroupVars = new List<NetworkIntAttributeGroup>();
-                                NetworkIntAttributeGroup Group1 = FindNetVarsToAttribute<NetworkIntAttributeGroup>(iter.AttributeGroupName);
+                                //NetworkIntAttributeGroup Group1 = FindNetVarsToAttribute<NetworkIntAttributeGroup>(iter.AttributeGroupName);
+                                NetworkIntAttributeGroup Group1 = null;
                                 bool isHasNetVar = Group1 != null;
                                 if (Group1 == null)
                                 {
                                     Group1 = new NetworkIntAttributeGroup();
+                                    /*
                                     if (NetworkAttributeGroupMap == null)
                                         NetworkAttributeGroupMap = new Dictionary<string, INetworkAttributeGroup>();
                                     NetworkAttributeGroupMap[iter.AttributeGroupName] = Group1;
+                                    */
                                 }
                                 Group1.Initialize(this);
                                 Group1.Name = iter.AttributeGroupName;
@@ -269,14 +278,17 @@ namespace SOC.GamePlay.Attribute
                             {
                                 if (NetworkInt64GroupVars == null)
                                     NetworkInt64GroupVars = new List<NetworkInt64AttributeGroup>();
-                                NetworkInt64AttributeGroup Group2 = FindNetVarsToAttribute<NetworkInt64AttributeGroup>(iter.AttributeGroupName);
+                                //NetworkInt64AttributeGroup Group2 = FindNetVarsToAttribute<NetworkInt64AttributeGroup>(iter.AttributeGroupName);
+                                NetworkInt64AttributeGroup Group2 = null;
                                 bool isHasNetVar = Group2 != null;
                                 if (Group2 == null)
                                 {
                                     Group2 = new NetworkInt64AttributeGroup();
+                                    /*
                                     if (NetworkAttributeGroupMap == null)
                                         NetworkAttributeGroupMap = new Dictionary<string, INetworkAttributeGroup>();
                                     NetworkAttributeGroupMap[iter.AttributeGroupName] = Group2;
+                                    */
                                 }
                                 Group2.Initialize(this);
                                 Group2.Name = iter.AttributeGroupName;
@@ -301,14 +313,17 @@ namespace SOC.GamePlay.Attribute
                             {
                                 if (NetworkStringGroupVars == null)
                                     NetworkStringGroupVars = new List<NetworkStringAttributeGroup>();
-                                NetworkStringAttributeGroup Group3 = FindNetVarsToAttribute<NetworkStringAttributeGroup>(iter.AttributeGroupName);
+                                //NetworkStringAttributeGroup Group3 = FindNetVarsToAttribute<NetworkStringAttributeGroup>(iter.AttributeGroupName);
+                                NetworkStringAttributeGroup Group3 = null;
                                 bool isHasNetVar = Group3 != null;
                                 if (Group3 == null)
                                 {
                                     Group3 = new NetworkStringAttributeGroup();
+                                    /*
                                     if(NetworkAttributeGroupMap == null)
                                         NetworkAttributeGroupMap = new Dictionary<string, INetworkAttributeGroup>();
                                     NetworkAttributeGroupMap[iter.AttributeGroupName] = Group3;
+                                    */
                                 }
                                 Group3.Initialize(this);
                                 Group3.Name = iter.AttributeGroupName;
@@ -330,6 +345,7 @@ namespace SOC.GamePlay.Attribute
                             break;
                     }
                 }
+                /*
                 if (NetworkAttributeGroupMap != null)
                 {
                     for (int i = 0; i < NetworkVariableFields.Count; ++i)
@@ -365,6 +381,7 @@ namespace SOC.GamePlay.Attribute
                         }
                     }
                 }
+                */
                 if (OnPostInitAttributeEvent != null)
                     OnPostInitAttributeEvent();
             }
