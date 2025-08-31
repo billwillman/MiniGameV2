68688fb926c324898af29f1e14c0cca9dd952fea
billwillman <billwillman@qq.com>
2025-08-31 23:33:03 +0800

    111

--- Assets/Script/WarFag/Editor/AutoTileEditorSceneRender.cs
+++ Assets/Script/WarFag/Editor/AutoTileEditorSceneRender.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-// 智能地表编辑器辅助显示（场景中渲染，测试用）
-public class AutoTileEditorSceneRender : Editor
+namespace AutoMap
 {
-    
+    // 智能地表编辑器辅助显示（场景中渲染，测试用）
+    [CustomEditor(typeof(AutoTileMap))]
+    public class AutoTileEditorSceneRender : Editor
+    {
+
+    }
 }
