e6baabb9dbebdb550a2aca3e54c9b0dcc1c37665
billwillman <billwillman@qq.com>
2025-09-01 01:48:43 +0800

    2222

--- Assets/Script/WarFag/Editor/AutoTileEditorSceneRender.cs
+++ Assets/Script/WarFag/Editor/AutoTileEditorSceneRender.cs
@@ -17,7 +17,7 @@ namespace AutoMap
             if (tileMap.m_AutoTileSize.x < 0 || Mathf.Abs(tileMap.m_AutoTileSize.x) <= float.Epsilon || tileMap.m_AutoTileSize.y < 0 || Mathf.Abs(tileMap.m_AutoTileSize.y) <= float.Epsilon)
                 return;
             var worldPos = tileMap.transform.position;
-            Handles.DrawWireCube(worldPos, new Vector3(tileMap.m_AutoTileSize.x, 0.01f, tileMap.m_AutoTileSize.y));
+            Handles.DrawWireCube(worldPos, new Vector3(tileMap.m_AutoTileSize.x, 1f, tileMap.m_AutoTileSize.y));
         }
     }
 }
