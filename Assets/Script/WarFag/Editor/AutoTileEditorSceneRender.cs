ff099bc13c3e763c7443225bad4bbdf472f9d663
billwillman <billwillman@qq.com>
2025-09-08 02:48:56 +0800

    111

--- Assets/Script/WarFag/Editor/AutoTileEditorSceneRender.cs
+++ Assets/Script/WarFag/Editor/AutoTileEditorSceneRender.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.Tilemaps;
+using UnityEngine.U2D;
 
 namespace AutoMap
 {
@@ -9,6 +12,15 @@ namespace AutoMap
     [CustomEditor(typeof(AutoTileMap))]
     public class AutoTileEditorSceneRender : Editor
     {
+
+        void DrawTileMapArea(AutoTileMap tileMap)
+        {
+            if (tileMap.m_AutoTileSize.x < 0 || Mathf.Abs(tileMap.m_AutoTileSize.x) <= float.Epsilon || tileMap.m_AutoTileSize.y < 0 || Mathf.Abs(tileMap.m_AutoTileSize.y) <= float.Epsilon)
+                return;
+            var worldPos = tileMap.transform.position;
+            Handles.DrawWireCube(worldPos, new Vector3(tileMap.m_AutoTileSize.x, 1f, tileMap.m_AutoTileSize.y));
+        }
+
         private void OnSceneGUI()
         {
             var tileMap = this.target as AutoTileMap;
@@ -16,8 +28,100 @@ namespace AutoMap
                 return;
             if (tileMap.m_AutoTileSize.x < 0 || Mathf.Abs(tileMap.m_AutoTileSize.x) <= float.Epsilon || tileMap.m_AutoTileSize.y < 0 || Mathf.Abs(tileMap.m_AutoTileSize.y) <= float.Epsilon)
                 return;
-            var worldPos = tileMap.transform.position;
-            Handles.DrawWireCube(worldPos, new Vector3(tileMap.m_AutoTileSize.x, 1f, tileMap.m_AutoTileSize.y));
+            DrawTileMapArea(tileMap);
+            DrawTileWire(tileMap);
+        }
+
+        void DrawTileWire(AutoTileMap tileMap)
+        {
+
+        }
+
+        private Rect[] m_SpriteDatas = null;
+
+        private static readonly Vector2 _cDrawStartPt = new Vector2(20, 100);
+
+        void DrawSpriteData(int index, Texture tex, int row, int col, Rect lastDrawRect)
+        {
+            if (m_SpriteDatas == null || m_SpriteDatas.Length <= 0 || index < 0 || index >= m_SpriteDatas.Length)
+                return;
+            var r = m_SpriteDatas[index];
+            float left = col * r.width;
+            float top = row * r.height;
+            Rect drawRect = new Rect(_cDrawStartPt.x + left, _cDrawStartPt.y + top, r.width, r.height);
+            Rect uvRect = new Rect(r.xMin / tex.width, r.yMin / tex.height, r.width / tex.width, r.height / tex.height);
+            GUI.DrawTextureWithTexCoords(drawRect, tex, uvRect);
+
+            var oldColor = GUI.contentColor;
+            GUI.contentColor = Color.black;
+            GUI.Label(drawRect, index.ToString());
+            GUI.contentColor = oldColor;
+        }
+
+        Rect[] GetSpriteDatas(AutoTileMap tileMap)
+        {
+            if (m_SpriteDatas == null && tileMap.m_TileAsset != null)
+            {
+                if (tileMap.m_TileAsset != null)
+                {
+                    string path = AssetDatabase.GetAssetPath(tileMap.m_TileAsset);
+                    if (!string.IsNullOrEmpty(path))
+                    {
+                        var importer = TextureImporter.GetAtPath(path) as TextureImporter;
+                        var metaDatas = importer.spritesheet;
+                        if (metaDatas != null && metaDatas.Length > 0)
+                        {
+                            m_SpriteDatas = new Rect[metaDatas.Length];
+                            foreach (var r in metaDatas)
+                            {
+                                int idx = int.Parse(r.name);
+                                m_SpriteDatas[idx] = r.rect;
+                            }
+                        }
+                    }
+                }
+            }
+            return m_SpriteDatas;
+        }
+
+        void ResetSprite()
+        {
+            m_SpriteDatas = null;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            // base.OnInspectorGUI();
+            var tileMap = this.target as AutoTileMap;
+            if (tileMap == null)
+                return;
+            tileMap.m_AutoTileSize = EditorGUILayout.Vector2Field("地图大小", tileMap.m_AutoTileSize);
+            tileMap.m_PerTileSize = EditorGUILayout.Vector2Field("瓦片大小", tileMap.m_PerTileSize);
+            var newSprite = EditorGUILayout.ObjectField("瓦片资源", tileMap.m_TileAsset, typeof(Sprite), false) as Sprite;
+            if (tileMap.m_TileAsset != newSprite)
+            {
+                tileMap.m_TileAsset = newSprite;
+                ResetSprite();
+            }
+            if (tileMap.m_TileAsset != null)
+            {
+                var texture = tileMap.m_TileAsset.texture;
+                if (texture != null)
+                {
+                    var lastRect = GUILayoutUtility.GetLastRect();
+                    var Rects = GetSpriteDatas(tileMap);
+                    DrawSpriteData(4, texture, 0, 0, lastRect);
+                    DrawSpriteData(12, texture, 0, 1, lastRect);
+                    DrawSpriteData(8, texture, 0, 2, lastRect);
+                    DrawSpriteData(5, texture, 1, 0, lastRect);
+                    DrawSpriteData(15, texture, 1, 1, lastRect);
+                    DrawSpriteData(10, texture, 1, 2, lastRect);
+                    DrawSpriteData(1, texture, 2, 0, lastRect);
+                    DrawSpriteData(3, texture, 2, 1, lastRect);
+                    DrawSpriteData(2, texture, 2, 2, lastRect);
+                    EditorGUILayout.GetControlRect(GUILayout.Height(30 * 9));
+                }
+            }
         }
     }
 }
