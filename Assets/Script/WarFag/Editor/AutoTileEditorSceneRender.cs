aac9643d52f6dfd2cf6d31c9ce9dc52c13a1284e
billwillman <billwillman@qq.com>
2025-09-16 02:40:30 +0800

    1111

--- Assets/Script/WarFag/Editor/AutoTileEditorSceneRender.cs
+++ Assets/Script/WarFag/Editor/AutoTileEditorSceneRender.cs
@@ -363,9 +363,6 @@ namespace AutoMap
             if (GUILayout.Button("清理Cell数据"))
             {
                 tileMap.ClearTileCells();
-                var sceneView = SceneView.currentDrawingSceneView;
-                if (sceneView != null)
-                    sceneView.Repaint();
             }
         }
 
