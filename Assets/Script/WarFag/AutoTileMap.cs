1dda6bfe3e5450b0f0f35cce1856ea8e5a66ead4
billwillman <billwillman@qq.com>
2025-09-21 18:50:25 +0800

    2211

--- Assets/Script/WarFag/AutoTileMap.cs
+++ Assets/Script/WarFag/AutoTileMap.cs
@@ -247,6 +247,12 @@ namespace AutoMap
                                         CompareCellValue(globalR, globalC - 1, null, 5, 7, 13, 15) && CompareCellValue(globalR, globalC + 1, null, 8, 9, 12, 13) &&
                                         CompareCellValue(globalR - 1, globalC - 1, null, 1, 3, 5, 7, 9, 11, 13, 15) && CompareCellValue(globalR + 1, globalC - 1, null, 4, 5, 6, 7, 12, 13, 14, 15) && CompareCellValue(globalR - 1, globalC + 1, null, 2, 3, 6, 7, 10, 11, 14, 15))
                                         isVaild = true;
+                                } else if (targetValue == 13)
+                                {
+                                    if (CompareCellValue(globalR + 1, globalC, null, 4, 5, 6, 7) && CompareCellValue(globalR - 1, globalC, null, 3, 7, 11, 15) &&
+                                        CompareCellValue(globalR, globalC - 1, null, 4, 6, 12, 14) && CompareCellValue(globalR, globalC + 1, null, 10, 11, 14, 15)
+                                        )
+                                        isVaild = true;
                                 }
                                 /*else if (targetValue == 6)
                                 {
@@ -270,13 +276,8 @@ namespace AutoMap
                                         GetCellValue(globalR + 1, globalC) != 0 && GetCellValue(globalR, globalC + 1) != 0 &&
                                         GetCellValue(globalR - 1, globalC) != 0 && GetCellValue(globalR + 1, globalC + 1) != 0)
                                         isVaild = true;
-                                }  else if (targetValue == 13)
-                                {
-                                    if (CompareCellValue(globalR + 1, globalC - 1, null, 0, 2) &&
-                                        GetCellValue(globalR + 1, globalC) != 0 && GetCellValue(globalR, globalC + 1) != 0 &&
-                                        GetCellValue(globalR - 1, globalC) != 0 && GetCellValue(globalR - 1, globalC + 1) != 0)
-                                        isVaild = true;
-                                }*/
+                                }  
+                                */
                                 else
                                     isVaild = true;
                             }
