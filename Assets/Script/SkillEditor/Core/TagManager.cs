a4152a925a934272489fa3fef221774bcb3b3e7d
billwillman <billwillman@qq.com>
2025-10-26 04:25:12 +0800

    2212

---
+++ Assets/Script/SkillEditor/Core/TagManager.cs
@@ -0,0 +1,25 @@
+using SOC.GamePlay;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GAS
+{
+    public class TagNode
+    {
+        public string name;
+        public TagRootNode rootNode = null;
+        public List<TagNode> childNode = null;
+    }
+
+    public class TagRootNode : TagNode
+    {
+        public ulong tagValue;
+    }
+
+    public class TagManager : ILuaBinder
+    {
+        public List<TagRootNode> m_TagRootNodes = null;
+    }
+}
