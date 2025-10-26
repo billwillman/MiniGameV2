a690a88b8bcb06cbbf1b04d72958511bdc849e2c
billwillman <billwillman@qq.com>
2025-10-26 22:18:46 +0800

    22211

--- Assets/Script/SkillEditor/Core/TagManager.cs
+++ Assets/Script/SkillEditor/Core/TagManager.cs
@@ -17,12 +17,15 @@ namespace GAS
 
     [System.Serializable]
     public class TagRootNode : TagNode
-    {}
+    {
+        public ulong id; // 枚举
+    }
 
     [Tooltip("GameTag总定义")]
     public class TagManager : SingetonMono<TagManager>
     {
         public List<TagRootNode> m_TagRootNodes = null;
-        private Dictionary<string, TagRootNode> m_TagMap = new Dictionary<string, TagRootNode>();
+        private Dictionary<string, TagNode> m_TagMap = new Dictionary<string, TagNode>();
+        private Dictionary<ulong, TagRootNode> m_TagRootMap = new Dictionary<ulong, TagRootNode>(); // 枚举对应的TagRootNode
     }
 }
