0f63d39b94c7595329abe3a73d1af0baebc88d06
billwillman <billwillman@qq.com>
2025-10-28 02:24:35 +0800

    2231231

--- Assets/Script/SkillEditor/Core/GameTags.cs
+++ Assets/Script/SkillEditor/Core/GameTags.cs
@@ -5,6 +5,7 @@ using Utils;
 using Netcode;
 using Unity.Netcode;
 using SOC.GamePlay;
+using SOC.GamePlay.Attribute;
 
 namespace GAS
 {
@@ -12,17 +13,41 @@ namespace GAS
     public class GameTags : NetworkBehaviour
     {
         // 动态的
-        private NetworkList<ulong> m_TagValues = new NetworkList<ulong>();
+        private NetworkVariable<Int64AttributeGroup> m_TagValues = new NetworkVariable<Int64AttributeGroup>();
 
         private void Awake()
         {
+            m_TagValues.bRepNotify = true;
             if (!GameStart.IsDS)
-                m_TagValues.OnListChanged += OnRep_TagValues;
+            {
+                m_TagValues.OnValueChanged = OnRep_TagValues;
+            }
         }
 
-        void OnRep_TagValues(NetworkListEvent<ulong> evt)
-        {}
+        void OnRep_TagValues(Int64AttributeGroup oldValue, Int64AttributeGroup newValue)
+        { }
 
-        
+        public bool GetTagValue(ushort enumId, out long tagValue)
+        {
+            return m_TagValues.Value.AttributeMap.TryGetValue(enumId, out tagValue);
+        }
+
+        // 是否有这个TAG
+        public bool HasTag(ushort enumId, string tagPath)
+        {
+            if (string.IsNullOrEmpty(tagPath))
+                return false;
+            long tagMask;
+            if (!GetTagValue(enumId, out tagMask))
+                return false;
+            var instance = TagManager.GetInstance();
+            if (instance == null)
+                return false;
+            var tagNode = instance.GetTagNode(tagPath);
+            if (tagNode == null)
+                return false;
+            bool ret = ((ulong)tagMask & tagNode.mask) != 0;
+            return ret;
+        }
     }
 }
