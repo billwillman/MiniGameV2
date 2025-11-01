c7932524ca19b4407e12c16b49ffa57b2fddc513
billwillman <billwillman@qq.com>
2025-11-02 05:06:39 +0800

    1111222

--- Assets/Script/SkillEditor/Core/BaseTask.cs
+++ Assets/Script/SkillEditor/Core/BaseTask.cs
@@ -5,14 +5,75 @@ using UnityEngine;
 
 namespace GAS
 {
+    public enum TaskResultState
+    {
+        Default = 0,
+        Success = 1,
+        Fail = -1
+    }
+
     // 基本类
     //[Attachable(typeof(CommonGroup))]
     //[Name("BaseTask")]
     public abstract class BaseTask: CutsceneTrack
     {
         public bool CanTimeSkip = false; // 能否时间过了跳过，默认不可以
+        public bool IsRunOnce; // 是否只执行一次
+        public uint RunedTimes = 0; // 执行了几次
 
-        public TagManager m_TagManager = null; // 标记
+        public TaskResultState ResultState
+        {
+            get;
+            set;
+        }
+
+        private GameTags m_TagRef = null;
+        public GameTags Tags
+        {
+            get
+            {
+                if (m_TagRef != null)
+                    return m_TagRef;
+                if (actor != null)
+                {
+                    m_TagRef = actor.GetComponent<GameTags>();
+                }
+                return m_TagRef;
+            }
+        }
+
+        // 依赖项
+        public List<BaseCondition> PreConditions
+        {
+            get;
+            set;
+        }
+
+        public bool IsOKByPreConditions()
+        {
+            if (PreConditions == null || PreConditions.Count <= 0)
+                return true;
+            for (int i = 0; i < PreConditions.Count; ++i)
+            {
+                var condtion = PreConditions[i];
+                if (condtion != null)
+                {
+                    if (!condtion.IsConditionOK())
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsTaskTimesNoCanRun()
+        {
+            if (IsRunOnce)
+            {
+                if (RunedTimes > 0)
+                    return false;
+            }
+            return true;
+        }
 
         public abstract void OnTaskBegin();
         public abstract void OnTaskEnd();
@@ -23,14 +84,25 @@ namespace GAS
             float currenTime = this.CurrentTime;
             if (currenTime >= 0 && (!this.CanTimeSkip || currenTime < this.endTime))
             {
-                OnTaskBegin();
+                if (IsTaskTimesNoCanRun() && IsOKByPreConditions())
+                {
+                    IsStartRuning = true;
+                    ++RunedTimes;
+                    OnTaskBegin();
+                }
             }
         }
+
+        protected bool IsStartRuning = false;
 
         protected override void OnExit()
         {
             base.OnExit();
-            OnTaskEnd();
+            if (IsStartRuning)
+            {
+                OnTaskEnd();
+                IsStartRuning = false;
+            }
         }
 
         public Cutscene Scene
---
+++ Assets/Script/SkillEditor/Task/JumpTask.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GAS
+{
+    public class JumpTask : BaseTask
+    {
+        // 跳转到开头
+        public float ToJumpTimeFromStart = 0f;
+
+        public override void OnTaskBegin()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnTaskEnd()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
